3ccb0e3a71e6a188c06b03d556734448d93e415f
Wassim Ferchichi <wassim.ferchichi@ept.tn>
2026-07-12 08:59:58 +0200

    chore: clean up temporary files

---
+++ scripts/test_ingestion.py
@@ -0,0 +1,14 @@
+﻿import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+
+from src.ingestion.loader import load_pdf
+from src.ingestion.chunker import chunk_documents
+
+docs = load_pdf('data/raw/test.pdf')
+print(f'Pages chargees : {len(docs)}')
+
+chunks = chunk_documents(docs)
+print(f'Chunks generes : {len(chunks)}')
+print(f'Chunk 0 : {chunks[0][chr(39)+"text"+chr(39)][:150]}')
+print(f'Metadata : {chunks[0][chr(39)+"metadata"+chr(39)]}')
